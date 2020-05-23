ca36987e001a97073ca9ea872993a28b506bc909
Leooman <351234955@qq.com>
2020-05-23 14:14:27 +0800

    add global scss variables

---
+++ components/workbench.vue
@@ -0,0 +1,55 @@
+<template>
+  <section id="workbench">
+    <section class="ASIDECONTAINER">
+      <slot name="aside"></slot>
+    </section>
+    <section class="MAINCONTAINER">
+      <section class="WORKCONTAINER">
+        <slot name="work"></slot>
+      </section>
+      <section class="LOGCONTAINER">
+        <slot name="log"></slot>
+      </section>
+    </section>
+    <section class="HELPCONTAINER">
+      <slot name="help"></slot>
+    </section>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+#workbench {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  overflow: hidden;
+  .ASIDECONTAINER {
+    flex: 0 0 100px;
+    height: 100%;
+    background: #cccccc;
+  }
+  .MAINCONTAINER {
+    flex: 1 1 auto;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    .WORKCONTAINER {
+      flex: 1 1 auto;
+      overflow: auto;
+      background: #eeeeee;
+    }
+    .LOGCONTAINER {
+      flex: 0 0 100px;
+      background: #aaaaaa;
+    }
+  }
+  .HELPCONTAINER {
+    flex: 0 0 100px;
+    height: 100%;
+    background: #cccccc;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,15 @@
 <template>
-  <section id="workbench">
-    <section class="ASIDECONTAINER">操作区</section>
-    <section class="MAINCONTAINER">
-      <section class="WORKCONTAINER">
-        工作区
-      </section>
-      <section class="LOGCONTAINER">通知区</section>
-    </section>
-    <section class="HELPCONTAINER">帮助区</section>
-  </section>
+  <workbench>
+    <template slot="work">test</template>
+  </workbench>
 </template>
 
 <script>
+import workbench from "@/components/workbench"
 export default {
-  name: "Layout",
+  components:{
+    workbench
+  },
   data() {
     return {};
   },
@@ -23,38 +19,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#workbench {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: flex-start;
-  justify-content: flex-start;
-  overflow: hidden;
-  .ASIDECONTAINER {
-    flex: 0 0 200px;
-    height: 100%;
-    background: #cccccc;
-  }
-  .MAINCONTAINER {
-    flex: 1 1 auto;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
-    .WORKCONTAINER {
-      flex: 1 1 auto;
-      overflow: auto;
-      background: #eeeeee;
-    }
-    .LOGCONTAINER {
-      flex: 0 0 100px;
-      background: #aaaaaa;
-    }
-  }
-  .HELPCONTAINER {
-    flex: 0 0 200px;
-    height: 100%;
-    background: #cccccc;
-  }
-}
+
 </style>
